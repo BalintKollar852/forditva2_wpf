e06e844fe8ec5790eda29727b222768898e96ecf
Torrente <71377802+BalintKollar852@users.noreply.github.com>
2023-09-12 11:19:17 +0200

    asd

--- forditva2/MainWindow.xaml.cs
+++ forditva2/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace forditva2
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
